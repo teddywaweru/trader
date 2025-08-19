c1791ff0e6ab66373d6c5918a11fcbddac6f27b7
Teddy Njuguna <teddywaweru@gmail.com>
2025-08-18 23:14:31 -0500

    feat: calculate atr for OHLC

--- algo/src/lib.rs
+++ algo/src/lib.rs
@@ -1,3 +1,4 @@
+use mt5::OHLC;
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -11,4 +12,25 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+}
+
+pub fn calculate_atr(ticks: &Vec<OHLC>) -> f32 {
+    //caclulate average TR for all data, store the last one to use later
+    // Calculate ATR as ( Prev_ATR(n-1) + last_ATR ) / n
+
+    let mut true_ranges: Vec<f32> = vec![];
+    for (idx, tick) in ticks.iter().enumerate() {
+        if idx == 0 {
+            continue;
+        }
+        let h_l = f32::abs(tick.high - tick.low);
+        let h_cp = f32::abs(tick.high - ticks[idx - 1].close);
+        let l_cp = f32::abs(tick.low - ticks[idx - 1].close);
+
+        let true_range = f32::max(f32::max(h_l, h_cp), l_cp);
+        true_ranges.push(true_range);
+    }
+    let atr = true_ranges.iter().sum::<f32>() / true_ranges.len() as f32;
+
+    atr
 }

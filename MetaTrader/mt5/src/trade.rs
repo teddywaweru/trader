e90d6fc08816e13dc338d5ef28079730486a5bba
Teddy Njuguna <teddywaweru@gmail.com>
2025-09-30 16:29:27 -0500

    fix: naming convention for struct field to match MT5

--- MetaTrader/mt5/src/trade.rs
+++ MetaTrader/mt5/src/trade.rs
@@ -1,5 +1,5 @@
 use crate::serde_order_type;
-use crate::{OrderType, Symbol};
+use crate::{Mt5Bridge, OrderType, Symbol};
 use serde::{Deserialize, Serialize};
 use serde_json;
 
@@ -7,15 +7,15 @@ use serde_json;
 pub struct OpenTrade {
     ticket: u32,
     magic: i32,
-    symbol: Symbol,
-    lot_size: f32,
+    pub symbol_name: String,
+    volume: f32,
     #[serde(with = "serde_order_type")]
     trade_type: OrderType,
-    open_price: f32,
-    open_time: String,
+    price_open: f32,
+    time_open: String,
     sl: f32,
     tp: f32,
-    pnl: f32,
+    profit: f32,
     comment: String,
 }
 impl Default for OpenTrade {
@@ -23,14 +23,14 @@ impl Default for OpenTrade {
         Self {
             ticket: 123321,
             magic: 123321,
-            symbol: Symbol::default(),
-            lot_size: 0.01,
+            symbol_name: "".to_string(),
+            volume: 0.01,
             trade_type: OrderType::OrderNanDefault,
-            open_price: 0.0,
-            open_time: "Default time".to_string(),
+            price_open: 0.0,
+            time_open: "Default time".to_string(),
             sl: 500.0,
             tp: 500.0,
-            pnl: 0.0,
+            profit: 0.0,
             comment: "Default Comment".to_string(),
         }
     }
@@ -59,7 +59,7 @@ struct ClosedTrade {
     ticket: u32,
     magic: i32,
     symbol: Symbol,
-    lot_size: f32,
+    volume: f32,
     trade_type: OrderType,
     open_price: f32,
     close_price: f32,

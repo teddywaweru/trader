d55a6f5ef45b1a9146b96df6200a5a8a1be4ffd6
Teddy Njuguna <teddywaweru@gmail.com>
2025-10-29 00:34:30 -0500

    update

--- tui/src/opentrades.rs
+++ tui/src/opentrades.rs
@@ -30,14 +30,15 @@ impl Widget for OpenTradesWidget {
                     .iter()
                     .map(|trade| {
                         Line::from(format!(
-                            "{} \n, {}, {}",
+                            "{} \n, {:#?} {}, {}",
                             trade.symbol_name.clone(),
+                            trade.trade_type,
                             trade.profit,
                             trade.time_open
                         ))
                     })
                     .collect::<Vec<Line>>();
-                let mut line = Text::from("Symbol    Profit   Time Open");
+                let mut line = Text::from("Symbol       Type    Profit   Time      Open");
                 line.extend(open_trades_text);
                 line.extend(Text::from("-------------"));
                 line.extend(Text::from(format!(
@@ -49,6 +50,8 @@ impl Widget for OpenTradesWidget {
                         .iter()
                         .sum::<f32>()
                 )));
+                self.render_exposure(&mut line, &open_trades);
+                line.extend(Text::from("-------------"));
                 Paragraph::new(line)
                     .block(calculator_block)
                     .render(area, buf);
@@ -64,5 +67,51 @@ impl Widget for OpenTradesWidget {
                 .render(area, buf);
             }
         }
+    }
+}
+struct Exposure {
+    currency: String,
+    symbol: String,
+    profit: i32,
+    sl: i32,
+}
+impl OpenTradesWidget {
+    fn render_exposure(self, line: &mut Text, open_trades: &[OpenTrade]) {
+        let mut sell_exposures = vec![];
+        let mut buy_exposures = vec![];
+        open_trades
+            .iter()
+            .map(|trade| {
+                let (base_curr, quote_curr) = trade.symbol_name.split_at(3);
+                match trade.trade_type {
+                    mt5::OrderType::Buy => {
+                        if !buy_exposures.contains(&quote_curr) {
+                            buy_exposures.push(quote_curr);
+                        }
+                    }
+                    mt5::OrderType::Sell => {
+                        if !sell_exposures.contains(&base_curr) {
+                            sell_exposures.push(base_curr);
+                        }
+                    }
+                    _ => {}
+                };
+            })
+            .for_each(drop);
+        line.extend(Line::from("------- Exposure"));
+        line.extend(Line::from("Buy Exposures"));
+        line.extend(
+            buy_exposures
+                .iter()
+                .map(|curr| Line::from(curr.to_string()))
+                .collect::<Vec<Line>>(),
+        );
+        line.extend(Line::from("Sell Exposures"));
+        line.extend(
+            sell_exposures
+                .iter()
+                .map(|curr| Line::from(curr.to_string()))
+                .collect::<Vec<Line>>(),
+        );
     }
 }

13d0c6a132f065b099822d1f5ba82fb48bc6674f
Teddy Njuguna <teddywaweru@gmail.com>
2025-10-29 00:18:31 -0500

    update account tui

--- tui/src/account.rs
+++ tui/src/account.rs
@@ -10,20 +10,17 @@ impl Default for AccountWidget {
         Self {}
     }
 }
-impl AccountWidget{
-
-}
+impl AccountWidget {}
 impl Widget for AccountWidget {
     fn render(self, area: Rect, buf: &mut Buffer)
     where
         Self: Sized,
     {
-
+        let account = mt5::Account::get_account_info();
         // let symbols = Symbol::get_all("mt5");
-
         let currencies_block = Block::bordered()
             .title("Account Details")
-            .title_top("Gotcha yah!")
+            .title_top(format!("{}: {}: {}", account.name, account.account_number, account.currency))
             .title_alignment(Alignment::Center)
             .border_set(border::DOUBLE);
         // let currency_text = symbols
@@ -31,9 +28,16 @@ impl Widget for AccountWidget {
         //     .map(|symbol| Line::from(format!("{:#?}", symbol.name)).alignment(Alignment::Right))
         //     .collect::<Vec<Line>>();
 
-        let mut text = Text::from_iter([Line::from("Currencies").alignment(Alignment::Center)]);
-        // text.extend(currency_text);
-        Paragraph::new(text)
+        let line = Text::from_iter([
+            Line::from(format!("Current Balance: {}", account.current_profit)),
+            Line::from(format!("Current Equity: {}", account.current_equity)),
+            Line::from(format!("Current Profit: {}", account.current_profit)),
+            Line::from(format!("Leverage: {}", account.leverage)),
+            Line::from(format!("Free Margin: {}", account.free_margin)),
+            Line::from(format!("Current Time: {}", account.current_time)),
+        ]);
+
+        Paragraph::new(line)
             .centered()
             .block(currencies_block)
             .render(area, buf);
